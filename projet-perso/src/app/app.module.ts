90c5ed2be01a88e9112c8cbe6c776c248f21474e
Valentinpjde <vpoujade@unigridsolutions.com>
2020-03-16 16:39:31 +0100

    Start sidebar

--- projet-perso/src/app/app.module.ts
+++ projet-perso/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 @NgModule({
   declarations: [
     AppComponent,
