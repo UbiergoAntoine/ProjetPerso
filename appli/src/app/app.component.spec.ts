f0eb45c99c9b44f577f9e94abed16dc6a380ecbb
Antoine Ubiergo <antoine.ubiergo@sparte.io>
2020-03-17 13:36:15 +0100

    Init avec tous les material modules

--- appli/src/app/app.component.spec.ts
+++ appli/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projet-perso'`, () => {
+  it(`should have as title 'appli'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projet-perso');
+    expect(app.title).toEqual('appli');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('projet-perso app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('appli app is running!');
   });
 });
