ebac94b8f99b75189262a0311d7644d2fee72700
Antoine Ubiergo <antoine.ubiergo@sparte.io>
2020-03-17 14:14:20 +0100

    init on master

--- appli/src/app/app-routing.module.ts
+++ appli/src/app/app-routing.module.ts
@@ -1,28 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-<<<<<<< HEAD:projet-perso/src/app/app-routing.module.ts
-import { SigninComponent } from './components/signin/signin.component';
-=======
->>>>>>> devAntoine:appli/src/app/app-routing.module.ts
 
 const routes: Routes = [
-  // On commence par la route du SignIn
-  { path: 'auth/signin', component: SigninComponent },
-  // Si SignIn Successful il route vers Home
-  { path: 'posts', component: HomeComponent },
-  // { path: 'posts/edit/:id', component: PostEditComponent },
-  // { path: 'posts/view/:id', component: PostSingleComponent },
-
-<<<<<<< HEAD:projet-perso/src/app/app-routing.module.ts
-  // Undefiened (on peut faire avec _redirects )
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', redirectTo: 'home' }];
-=======
-const routes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
 ];
->>>>>>> devAntoine:appli/src/app/app-routing.module.ts
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
