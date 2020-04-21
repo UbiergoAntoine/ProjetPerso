d18cf12a294cfb5aca10e01ae6a95141e1bdb039
Antoine Ubiergo <antoine.ubiergo@sparte.io>
2020-04-21 15:50:10 +0200

    netlify fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,21 +10,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 
-// Dans _redirects
-// * indique que pour toutes les routes undefiened, on retourne vers index.html et 200 est une sorte d'indicateur (on aurait pu mettre une string)
-
-
 const routes: Routes = [
   { path: 'auth/signin', component: SigninComponent },
   { path: 'posts', component: PostListComponent },
   { path: 'posts/edit/:id', component: PostEditComponent },
   { path: 'posts/view/:id', component: PostSingleComponent },
-
   { path: 'home', component: HomeComponent },
-
-  // Undefiened
-  // Du coup c'est useless si on a _redirects
-
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home' }];
 
